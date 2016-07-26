1d103fdf24a49230479de9c5ba410f5f17f9d8ae
yoshitomo agou <y0.ag0u3@gmail.com>
2016-07-26 23:18:37 +0900

    プロジェクト ファイルを追加します。

---
+++ CamAppDemo01/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// add
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using System.Diagnostics;
+using Windows.Media.Capture;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+namespace CamAppDemo01
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // MediaCapture and its state variables
+        private MediaCapture _mediaCapture;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await InitializeCameraAsync();
+        }
+
+        private async Task InitializeCameraAsync()
+        {
+            var cameraDevice = await FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel.Back);
+
+            if (cameraDevice == null)
+            {
+                Debug.WriteLine("No camera device found!");
+                return;
+            }
+
+            // Create MediaCapture and its settings
+            _mediaCapture = new MediaCapture();
+
+
+            var settings = new MediaCaptureInitializationSettings { VideoDeviceId = cameraDevice.Id };
+
+            // Initialize MediaCapture
+            try
+            {
+                await _mediaCapture.InitializeAsync(settings);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Debug.WriteLine("The app was denied access to the camera");
+            }
+
+            await StartPreviewAsync();
+        }
+
+        private async Task StartPreviewAsync()
+        {
+            // Set the preview source in the UI and mirror it if necessary
+            PreviewControl.Source = _mediaCapture;
+
+            // Start the preview
+            await _mediaCapture.StartPreviewAsync();
+        }
+
+
+        private static async Task<DeviceInformation> FindCameraDeviceByPanelAsync(Windows.Devices.Enumeration.Panel desiredPanel)
+        {
+            // Get available devices for capturing pictures
+            var allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+            // Get the desired camera by panel
+            DeviceInformation desiredDevice = allVideoDevices.FirstOrDefault(x => x.EnclosureLocation != null && x.EnclosureLocation.Panel == desiredPanel);
+
+            // If there is no device mounted on the desired panel, return the first device found
+            return desiredDevice ?? allVideoDevices.FirstOrDefault();
+        }
+    }
+}
